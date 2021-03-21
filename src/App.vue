64e31070eb113efde2aeb2799c9a5f8905d0d609
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-03-21 03:11:43 +0200

    create a custom resource to use it in a differant places

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,8 @@ export default {
       //       console.log(error);
       //     });
 
-      this.resource.save({}, this.user);
+      // this.resource.save({}, this.user);
+      this.resource.saveAlt(this.user);
     },
     fetchData(){
       this.$http.get('data.json')
@@ -69,7 +70,11 @@ export default {
     }
   },
     created(){
-      this.resource = this.$resource('data.json');
+      const customActions = {
+        saveAlt: {method:'POST', url:'alternative.json'}
+      }
+
+      this.resource = this.$resource('data.json',{}, customActions);
     }
   
 }
